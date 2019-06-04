da3cc2eb7ecf529e5839e17489beded0464de280
gelzki <leoangelodiamat@gmail.com>
2019-06-04 10:48:57 +0800

    Done Dashboard

--- phishingquiz/settings.py
+++ phishingquiz/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8g*r7zabs4^9#%_=4djxoype!b4kg$6h^namtcx)qkc3qn^*g%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
